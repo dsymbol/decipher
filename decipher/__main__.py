c691a6a0a77ff75cd8b8f47ee6a7b7ec72e452b4
dsymbol <dsymbol@users.noreply.github.com>
2022-12-28 01:09:46 +0200

    more descriptive cli switches

--- decipher/__main__.py
+++ decipher/__main__.py
@@ -4,7 +4,7 @@ import sys
 import click
 import whisper
 
-from decipher.actions import set_workdir, transcribe, subtitle
+from decipher.action import transcribe, subtitle
 from decipher.ffexec import get_ffmpeg_exe
 
 
@@ -15,34 +15,33 @@ def cli():
 
 @cli.command(name='transcribe', help='transcribe a video')
 @click.option('-i', '--input', required=True, prompt="Enter video file path", type=str, help='input video file path e.g. video.mp4')
-@click.option('-o', '--output', type=str, default='result', help='output directory path')
+@click.option('-o', '--output_dir', type=str, default='result', help='output directory path')
 @click.option('--model', default='small', help='name of the whisper model to use', type=click.Choice(whisper.available_models()))
 @click.option('--language', default=None, help='language spoken in the audio')
 @click.option('--task', default='transcribe', help='whether to perform X->X speech recognition (\'transcribe\') or X->English translation (\'translate\')', type=click.Choice(['transcribe', 'translate']))
-@click.option('--subs', '-s', default=None, help='whether to perform subtitle add or burn action', type=click.Choice(['add','burn']))
-def transcribe_cmd(input, output, model, language, task, subs):
+@click.option('-a', '--subtitle_action', default=None, help='whether to perform subtitle add or burn action', type=click.Choice(['add','burn']))
+def _transcribe_cli(input, output_dir, model, language, task, subtitle_action):
     transcribe(
-        os.path.abspath(input),
-        set_workdir(output),
+        input,
+        output_dir,
         model,
         language,
         task,
-        subs
+        subtitle_action
     )
 
 
 @cli.command(name='subtitle', help='subtitle a video')
 @click.option('-i', '--input', required=True, prompt="Enter video file path", type=str, help='input video file path e.g. video.mp4')
-@click.option('-o', '--output', type=str, default='result', help='output directory path')
-@click.option('--subs', '-s', prompt="Enter subtitles file path", required=True, type=str, help='input subtitles path e.g. subtitle.srt')
-@click.option('--task', default='burn', help='whether to perform subtitle add or burn action', type=click.Choice(['add','burn']))
-def subtitle_cmd(input, output, subs, task):
-    subs = os.path.abspath(subs)
+@click.option('-o', '--output_dir', type=str, default='result', help='output directory path')
+@click.option('-s', '--subtitle_file', prompt="Enter subtitles file path", required=True, type=str, help='input subtitles path e.g. subtitle.srt')
+@click.option('-a', '--subtitle_action', default='burn', help='whether to perform subtitle add or burn action', type=click.Choice(['add','burn']))
+def _subtitle_cli(input, output_dir, subtitle_file, subtitle_action):
     subtitle(
-        os.path.abspath(input),
-        set_workdir(output),
-        subs,
-        task
+        input,
+        output_dir,
+        subtitle_file,
+        subtitle_action
     )
 
 
