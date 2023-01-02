9684715391df0160c78e2aac1ece12e289af9bc6
dsymbol <88138099+dsymbol@users.noreply.github.com>
2023-01-02 15:18:02 +0000

    simplify setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='decipher',
     url='https://github.com/dsymbol/decipher',
-    license='OSI Approved :: MIT License',
     author='dsymbol',
-    author_email='',
-    platforms='OS Independent',
-    description='Effortlessly add whisper AI generated transcription subtitles into provided video',
     install_requires=[
         'whisper @ git+https://github.com/openai/whisper.git',
         'click==8.1.3',
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
     ],
     packages=find_packages(),
     entry_points={
